2e39247abe36b8c8fae0937e0f4b97bf95442561
omagejehosaphat <omage.prosper@gmail.com>
2023-11-23 06:05:30 +0100

    dependency downloads

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-blue-500  text-3xl' >here we go again </h1>
+
+      <h6 className='bg-amber-500'>we go dey alright </h6>
+    </>
+  )
+}
+
+export default App
