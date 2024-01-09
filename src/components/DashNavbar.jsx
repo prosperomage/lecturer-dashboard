9466bb216e478ff027021da63373bbc6bf7b9761
omagejehosaphat <omage.prosper@gmail.com>
2024-01-09 20:47:45 +0100

    search icon is inside the search field
    we finally put the search button inside the search input field

--- src/components/DashNavbar.jsx
+++ src/components/DashNavbar.jsx
@@ -16,13 +16,15 @@ const DashNavbar = () => {
         </div>
         {/* search bar */}
         <div>
-          <div className="flex items-center mt-4">
-            <AiOutlineSearch className="  w-6 h-6 " />
+          <div className="relative w-[340px] flex items-center mt-4">
             <input
               type="search"
-              className="px-4 py-2 w-[340px] border-gray-300  rounded-lg focus:ring-blue-500 focus:border-blue-500"
+              className="px-4 py-2  border-gray-300  rounded-lg focus:ring-blue-500 focus:border-blue-500"
               placeholder="Search"
             />
+            <div className="absolute inset-y-0 left-0 pl-3 flex  outline-dotted items-center pointer-events-none">
+              <AiOutlineSearch className=" mr-5 outline-double w-6 h-6 " />
+            </div>
           </div>
         </div>
         {/*  student logo */}
--- src/nots.md
+++ src/nots.md
@@ -3,3 +3,20 @@
 
 
       absolute bottom-4 left-1/2 outline outline-amber-500 transform -translate-x-1/2 flex space-x-2
+
+<!-- chatgpt code -->
+        
+
+
+
+
+     <div className="relative  outline-dotted w-[340px] flex items-center mt-4">
+            <input
+              type="search"
+              className="px-4 py-2  border-gray-300  rounded-lg focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Search"
+            />
+            <div className="absolute inset-y-0 left-0 pl-3 flex  outline-dotted items-center pointer-events-none">
+              <AiOutlineSearch className=" mr-10 outline-double w-6 h-6 " />
+            </div>
+          </div>
