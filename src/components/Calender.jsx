33b01a2beff02f0555f3ea43e94656828cdb10e0
omagejehosaphat <omage.prosper@gmail.com>
2024-01-10 16:30:27 +0100

    calender fixed
    we finally got the calender component  working

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import CalenderImg from "../assets/Light Mode.svg";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+// type ValuePiece = Date | null;
+
+// type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const Calender = () => {
+ 
   return (
-    <div className="">
-      
-      <img src={CalenderImg} alt="calenderImage" className="w-[17rem] h-[10rem]  ml-5 " />
+    // <div className="">
+    //   {/* <img src={CalenderImg} alt="calenderImage" className="w-[17rem] h-[10rem]  ml-5 " /> */}
+    //   <Calendar onChange={onChange} value={value} />
+    // </div>
+
+    <div className="bg-blue-500 rounded-xl">
+      <Calendar />
     </div>
   );
 };
